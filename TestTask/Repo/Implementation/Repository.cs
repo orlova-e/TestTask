e5d0ef0c64d4f704e5c69d8aaeabfe74723a8ccb
Elizaveta Orlova <orlovaev11@gmail.com>
2021-04-25 14:22:00 +0300

    Добавлены интерфейс и реализация IRepository

---
+++ TestTask/Repo/Implementation/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using TestTask.Repo.Interfaces;
+
+namespace TestTask.Repo.Implementation
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private ConcurrentDictionary<Guid, T> _dictionary { get; } = new();
+
+        public T GetEntry(Guid id)
+        {
+            _dictionary.TryGetValue(id, out T value);
+            return value;
+        }
+
+        public bool HasEntry(Guid id) => _dictionary.TryGetValue(id, out T value);
+
+        public T SetOrUpdateEntry(Guid id, T value)
+        {
+            _dictionary.TryGetValue(id, out T oldValue);
+
+            return _dictionary.AddOrUpdate(id,
+                id => value,
+                (id, oldValue) => value);
+        }
+    }
+}
---
+++ TestTask/Repo/Interfaces/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestTask.Repo.Interfaces
+{
+    /// <summary>
+    /// In-memory storage
+    /// </summary>
+    /// <typeparam name="T">Type of stored object</typeparam>
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Checks if the item exists in the storage
+        /// </summary>
+        /// <param name="id">Item id</param>
+        /// <returns><see langword="true"/> if the item exists; otherwise, <see langword="false"/></returns>
+        bool HasEntry(Guid id);
+
+        /// <summary>
+        /// Returns a value from the storage
+        /// </summary>
+        /// <param name="id">Item id</param>
+        /// <returns>Returns a value from the storage if the item exists; otherwise, <see langword="null"/></returns>
+        T GetEntry(Guid id);
+
+        /// <summary>
+        /// Sets or updates an item in the storage
+        /// </summary>
+        /// <param name="id">Item id</param>
+        /// <param name="value">Item</param>
+        /// <returns>Set or updated value</returns>
+        T SetOrUpdateEntry(Guid id, T value);
+    }
+}
