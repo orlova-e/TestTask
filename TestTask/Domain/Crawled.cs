89ada36204ccdff86210d025c4e25a274df5ac81
Elizaveta Orlova <orlovaev11@gmail.com>
2021-04-25 14:25:01 +0300

    Добавлена модель Crawled, представляющая обрабатываемый объект

---
+++ TestTask/Domain/Crawled.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTask.Domain
+{
+    public class Crawled
+    {
+        public string Url { get; set; }
+        public int Result { get; set; }
+        public ProcessStatus Status { get; set; }
+    }
+}
