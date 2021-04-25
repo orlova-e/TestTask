4a4cd4ede8a36eb5b7b8711b44473ae94c835ced
Elizaveta Orlova <orlovaev11@gmail.com>
2021-04-25 15:08:59 +0300

    Добавлены интерфейс и реализация ICrawlerService
    * изменен минимальный уровень логирования в appsettings.Development.json на "Error"

---
+++ TestTask/App/Implementation/CrawlerService.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TestTask.App.Interfaces;
+using TestTask.Domain;
+using TestTask.Repo.Interfaces;
+
+namespace TestTask.App.Implementation
+{
+    public class CrawlerService : ICrawlerService
+    {
+        private IRepository<Crawled> _repository { get; }
+        private IWebService _webService { get; }
+        private ILogger<CrawlerService> _logger { get; }
+
+        public CrawlerService(IRepository<Crawled> repository,
+            IWebService webService,
+            ILogger<CrawlerService> logger)
+        {
+            _repository = repository;
+            _webService = webService;
+            _logger = logger;
+        }
+
+        public Guid StartProcessing(string url)
+        {
+            Guid id = Guid.NewGuid();
+            Crawled crawled = new()
+            {
+                Url = url,
+                Status = ProcessStatus.Processing,
+                Result = -1
+            };
+
+            _repository.SetOrUpdateEntry(id, crawled);
+            BackgroundCount(id, crawled);
+
+            return id;
+        }
+
+        public ProcessStatus GetStatus(Guid id)
+        {
+            if (!_repository.HasEntry(id))
+            {
+                return ProcessStatus.WithAnError;
+            }
+
+            Crawled crawled = _repository.GetEntry(id);
+            return crawled.Status;
+        }
+
+        public int GetResult(Guid id) => _repository.GetEntry(id)?.Result ?? -1;
+
+        private void BackgroundCount(Guid id, Crawled crawled)
+            => Task.Run(async () =>
+            {
+                if (await _webService.TryGetPageAsync(crawled.Url))
+                {
+                    try
+                    {
+                        string text = _webService.GetText();
+
+                        text = Regex.Replace(text, @"([^\w-][^A-Za-zА-Яа-я]+)|[\d]|(- )", " ");
+
+                        crawled.Result = text.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)?.Length ?? default;
+                        crawled.Status = ProcessStatus.Successfully;
+                    }
+                    catch (Exception)
+                    {
+                        _logger.LogError($"Cannot parse a html document with id [{id}]."
+                            + Environment.NewLine
+                            + $"The resource was on the [{crawled.Url}]");
+
+                        crawled.Status = ProcessStatus.WithAnError;
+                        crawled.Result = -1;
+                    }
+                }
+                else
+                {
+                    crawled.Status = ProcessStatus.WithAnError;
+                    crawled.Result = -1;
+                }
+            });
+    }
+}
---
+++ TestTask/App/Interfaces/ICrawlerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TestTask.Domain;
+
+namespace TestTask.App.Interfaces
+{
+    /// <summary>
+    /// Processing an Crawler object and getting the result
+    /// </summary>
+    public interface ICrawlerService
+    {
+        /// <summary>
+        /// Starts processing the page
+        /// </summary>
+        /// <param name="url">Url of the page to be processed</param>
+        /// <returns>The Id, which can be used to find out the result</returns>
+        Guid StartProcessing(string url);
+
+        /// <summary>
+        /// Used to return the page processing state
+        /// </summary>
+        /// <param name="id">Id of the processed or finished result</param>
+        /// <returns>Page processing state</returns>
+        ProcessStatus GetStatus(Guid id);
+
+        /// <summary>
+        /// Used to return the result of page processing
+        /// </summary>
+        /// <param name="id">Id of the processed or finished result</param>
+        /// <returns>Page processing result</returns>
+        int GetResult(Guid id);
+    }
+}
