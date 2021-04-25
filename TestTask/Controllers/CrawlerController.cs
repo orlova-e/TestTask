4f0c4a448c0d96210cafaaa5547ecdc65ec91a9f
Elizaveta Orlova <orlovaev11@gmail.com>
2021-04-25 15:19:41 +0300

    Добавлен CrawlerController

---
+++ TestTask/Controllers/CrawlerController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using TestTask.App.Interfaces;
+using TestTask.Domain;
+using TestTask.Repo.Interfaces;
+
+namespace TestTask.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CrawlerController : ControllerBase
+    {
+        private ICrawlerService _crawlerService { get; }
+        private IRepository<Crawled> _repository { get; }
+
+        public CrawlerController(ICrawlerService crawlerService,
+            IRepository<Crawled> repository)
+        {
+            _crawlerService = crawlerService;
+            _repository = repository;
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Crawl([FromBody] string url)
+        {
+            Guid id = _crawlerService.StartProcessing(url);
+            return Ok(new { RequestId = id });
+        }
+
+        [HttpGet("[action]/{id:guid}")]
+        public IActionResult Status([FromRoute] Guid id)
+        {
+            if (!_repository.HasEntry(id))
+            {
+                return NotFound(id);
+            }
+
+            ProcessStatus status = _crawlerService.GetStatus(id);
+            return Ok(new { Status = (int)status });
+        }
+
+        [HttpGet("[action]/{id:guid}")]
+        public IActionResult Result([FromRoute] Guid id)
+        {
+            Crawled crawled = _repository.GetEntry(id);
+
+            if (crawled is null || crawled.Status is not ProcessStatus.Successfully)
+            {
+                return NotFound(id);
+            }
+
+            return Ok(new { WordCount = crawled.Result });
+        }
+    }
+}
